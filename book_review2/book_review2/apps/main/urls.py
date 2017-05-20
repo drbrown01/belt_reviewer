829d07618984301faea76a60b2f1fd0124308178
Dan <drbrown01@yahoo.com>
2017-05-20 12:03:35 -0500

    updated routes to include home/success

--- book_review2/book_review2/apps/main/urls.py
+++ book_review2/book_review2/apps/main/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
-    # url(r'^index/', views.index),
+    url(r'^index/', views.index),
     url(r'^home/$', views.home),
+    url(r'^home/success$', views.success),
     url(r'^success/$', views.success),
 ]
