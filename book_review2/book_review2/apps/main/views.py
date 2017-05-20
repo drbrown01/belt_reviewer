409b80ecc9b371067c1ee604f24ab896ccb5b654
Dan <drbrown01@yahoo.com>
2017-05-20 10:01:04 -0500

    added html and views

--- book_review2/book_review2/apps/main/views.py
+++ book_review2/book_review2/apps/main/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 def index(request):
     return render(request, "main/index.html")
 
 def home(request):
-    return render(request, "main/home.html")
+    return redirect(request, "main/home.html")
 
 def success(request):
     return render(request, 'main/success.html')
